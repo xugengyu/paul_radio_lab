874880e65ff3eea59fd294fcc7d66687691f076f
paulx <xgy620@proton.me>
2026-01-03 22:12:35 -0800

    started some custom modules

---
+++ content/util/pybeam/__init__.py
@@ -0,0 +1,1 @@
+from .pybeam import *
---
+++ content/util/pybeam/pybeam.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+def steervec():
+    '''
+    Docstring for steervec
+    '''
+    return None
+
+def lcmv_weights():
+    '''
+    Docstring for lcmv_weights
+    '''
+    return None
