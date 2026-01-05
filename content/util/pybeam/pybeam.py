dab8f2e9a8aea1026ad1c2cffd331fb3427ac0d8
Paul Xu <xgy620@proton.me>
2026-01-04 17:08:55 -0800

    added proper docstring for steervec

--- content/util/pybeam/pybeam.py
+++ content/util/pybeam/pybeam.py
@@ -1,13 +1,25 @@
+"""
+A collection of utility functions for spatial signal processing
+"""
+
 import numpy as np
+from numpy.typing import NDArray
 
-def steervec():
-    '''
-    Docstring for steervec
-    '''
-    return None
+def steervec(
+    pos_lda: NDArray[np.float64], 
+    angle: NDArray[np.float64], 
+) -> NDArray[np.float64]:
+    
+    """
+        Computes the steering vector, a.k.a. the array manifold vector of an array
+        whose elements are located at positions pos_lda.
 
-def lcmv_weights():
-    '''
-    Docstring for lcmv_weights
-    '''
+        Args:
+            pos_lda: A numpy array containing the positions of the array elements, in units of wavelengths.
+            angle: A numpy array containing the theta and phi angles for which to compute the steering vector.
+
+        Returns:
+            A numpy array containing the steering vectors corresponding to the specified angles.
+    """
+
     return None
